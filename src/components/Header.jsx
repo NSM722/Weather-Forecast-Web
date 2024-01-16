50eef394f78e7f6383321caf2347e62ceeed812a
Sakina Mbatha <sakina@natix.io>
2024-01-16 16:53:38 +0100

    refactor: add new components and styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/prop-types */
 function Header() {
-  return <h1 className="display-1 fw-bolder text-secondary text-decoration-underline">WEATHER FORECAST APP</h1>;
+  return <h1 className="display-1 fw-bolder text-secondary text-decoration-underline">WEATHER FORECAST</h1>;
 }
 
 export default Header;
--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -1,32 +1,43 @@
 /* eslint-disable react/prop-types */
 import moment from 'moment';
-function WeatherCard({ foreCast }) {
+function WeatherCard({ forecast, weatherData }) {
   return (
-    <div className='card-group m-4'>
-      {foreCast?.map((item) => (
-        <div className='card mb-3 w-50' key={item.dt}>
-          <img
-            src={`http://openweathermap.org/img/w/${item.weather?.[0]?.icon}.png`}
-            className='card-img-top'
-            alt={`${item.weather?.[0]?.icon}`}
-            style={{ maxWidth: '100%', height: 'auto', width: '33%', alignSelf: 'center' }}
-          />
-          <div className='card-body'>
-            <h5 className='card-title text-primary'>
-              Day:&nbsp;{moment(item.dt * 1000).format('DD MMM YYYY')}{' '}
-            </h5>
-            <p className='card-text'>Temp: {item.temp?.max}° celsius</p>
-            <p className='card-text'>Wind speed: {item?.speed}</p>
-            <p className='card-text'>Clouds: {item.clouds} </p>
-            <p className='card-text'>
-              <small className='text-muted'>
-                Description: {item.weather?.[0]?.description}
-              </small>
-            </p>
+    <>
+      <p className='display-6 fw-bold text-success'>{weatherData?.city?.name}--{weatherData?.city?.country}</p>
+      <div className='row row-cols-1 row-cols-md-3 g-4'>
+        {forecast?.map((item) => (
+          <div className='col' key={item.dt}>
+            <div className='card border-3 border border-success'>
+              <img
+                src={`http://openweathermap.org/img/w/${item.weather?.[0]?.icon}.png`}
+                className='card-img-top'
+                alt={`${item.weather?.[0]?.icon}`}
+                style={{
+                  maxWidth: '100%',
+                  height: 'auto',
+                  width: '30%',
+                  alignSelf: 'center',
+                }}
+              />
+              <div className='card-body'>
+                <h5 className='card-title text-primary'>
+                  Day:&nbsp;{moment(item.dt * 1000).format('DD MMM YYYY')}{' '}
+                </h5>
+                <p className='card-text'>Temp: {item.temp?.max}° celsius</p>
+                <p className='card-text'>Humidity: {item.humidity}%</p>
+                <p className='card-text'>Wind speed: {item.speed}</p>
+                <p className='card-text'>Clouds: {item.clouds} </p>
+                <p className='card-text'>
+                  <small className='text-muted text-truncate'>
+                    Description: {item.weather?.[0]?.description}
+                  </small>
+                </p>
+              </div>
+            </div>
           </div>
-        </div>
-      ))}
-    </div>
+        ))}
+      </div>
+    </>
   );
 }
 
