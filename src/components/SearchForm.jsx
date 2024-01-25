940a6558df0f793192e3c621dba8d22505a8b7ee
Sakina Mbatha <sakina@natix.io>
2024-01-25 14:08:09 +0100

    refactor: rename component

---
+++ src/components/SearchForm.jsx
@@ -0,0 +1,19 @@
+/* eslint-disable react/prop-types */
+function SearchForm({ handleSubmit, handleChange, query }) {
+  return (
+    <form onSubmit={handleSubmit} className='m-4'>
+      <input
+        type='text'
+        placeholder='Enter city'
+        value={query}
+        onChange={handleChange}
+        className='p-2 rounded'
+      />
+      <button type='submit' className='btn btn-outline-secondary mx-2'>
+        Get Weather
+      </button>
+    </form>
+  );
+}
+
+export default SearchForm;
